9c451c24016435c3558731cc33bc0e9d67d46012
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-21 19:20:21 -0500

    Modificando HeroPage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
@@ -8,11 +8,45 @@ export const HeroPage = () => {
 
   const hero = getHeroById(id);
 
+  const navigate = useNavigate();
+  const onNavigateBack = () => {
+    navigate(-1); //regresa a la pagina anterior que estaba
+  }
+
   if(!hero){
     return <Navigate to="/marvel"/>
   }
 
   return (
-    <h1>Hero</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>  
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego:</b> {hero.alter_ego} </li>
+          <li className="list-group-item"> <b>Publisher:</b> {hero.publisher} </li>
+          <li className="list-group-item"> <b>First appearance:</b> {hero.first_appearance} </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+         <button 
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+        >
+          Regresar
+         </button>
+
+      </div> 
+
+    </div>
   )
 }
