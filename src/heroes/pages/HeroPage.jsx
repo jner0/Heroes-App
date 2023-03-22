e3d8551fa2364fa93e180d739bd799e5a8135da4
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-21 19:04:32 -0500

    Leyendo argumentos de la URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,17 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const {id} = useParams(); //el ID viene de la ruta en donde se especifico /hero:id
+  //console.log(id);
+
+  const hero = getHeroById(id);
+
+  if(!hero){
+    return <Navigate to="/marvel"/>
+  }
+
   return (
     <h1>Hero</h1>
   )
