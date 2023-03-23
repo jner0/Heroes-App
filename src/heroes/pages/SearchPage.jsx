c5e0a4f844643d611b7b676c41b599dd1f5175af
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-22 20:41:18 -0500

    Agregando mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,6 +13,10 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search); //procesa los query apram de manera separada
   const heroes = getHeroesByName(q);
 
+  //para los mensajes condicionales de error y otro
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q
 
@@ -20,8 +24,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if(searchText.trim().length <= 1 ) return;
-
+    //if(searchText.trim().length <= 1 ) return;
     navigate(`?q=${searchText}`);
 
   }
@@ -58,14 +61,20 @@ export const SearchPage = () => {
                 <h4>Results</h4>
                 <hr />
 
-                <div className="alert alert-primary">
+                {/* {
+                  ( q === '' )
+                  ? <div className="alert alert-primary">Search a Hero</div>
+                  : ( heroes.length === 0 ) && <div className="alert alert-danger">No Hero with <b>{q}</b></div>
+                } */}
+
+                <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>
                   Search a Hero
                 </div>
 
-                <div className="alert alert-danger">
+                <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
                   No Hero with <b>{q}</b>
                 </div>
-
+                                
                 {
                   heroes.map( hero => (
                     <HeroCard key={ hero.id } { ...hero } />
