8bd546dfe9d791e7d2643a0dc98af56fa32ba266
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-23 22:04:43 -0500

    Creando proyeccion de rutas

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,19 @@
+//componente que sirve para proveer la informacion a toda la aplicacion
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./authReducer"
+
+const initialState = {
+    logged: false,
+}
+
+export const AuthProvider = ({ children }) => {
+
+   const [ state, dispatch ] =  useReducer( authReducer, initialState );
+
+  return (
+    <AuthContext.Provider value={{}}>
+        { children }
+    </AuthContext.Provider>
+  )
+}
---
+++ src/auth/types/types.js
@@ -0,0 +1,7 @@
+
+export const types = {
+    login: '[Auth] Login',
+    logout: '[Auth] Logout'
+}
+
+//El objetivo es tener de forma centralizada de tener los nombres de las acciones que voy a poder disparar, los posibles tipo
