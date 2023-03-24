f49cd73267c779d988fec9338128e5231885fccd
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-23 23:27:28 -0500

    Recordar ultima pagina visitada

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -9,9 +9,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
     login( 'Nemecio Rodriguez' );
 
-    navigate('/', {
+    navigate(lastPath, {
       replace: true //para no poder regresar
     });
   }
