318e11d27370bc633b81be0cc67b8c02ada1e455
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-23 23:06:38 -0500

    Rutas Privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
@@ -9,8 +10,14 @@ export const AppRouter = () => {
         <Routes>
             <Route path="login" element={<LoginPage/>} />
 
-            <Route path="/*" element={<HeroesRoutes/>}/>
 
+            {/* rutas privadas */}
+            <Route path="/*" element={
+              <PrivateRoute>
+                  <HeroesRoutes/>
+              </PrivateRoute>
+            }/>
+            {/* <Route path="/*" element={<HeroesRoutes/>}/> */}
         </Routes>
     </>
   )
